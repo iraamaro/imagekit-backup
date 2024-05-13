57647daa8d892f91b641ce3462c0082970e27b47
Ivan A <iramaro@disroot.org>
2024-05-13 17:50:53 -0300

    Init repo.

---
+++ backend/controllers/imagekit.ops.js
@@ -0,0 +1,78 @@
+import dotenv from 'dotenv'
+import axios from 'axios'
+import fs from 'fs'
+import ImageKit from 'imagekit'
+
+class ImageKitOps {
+    listFiles = async (skip) => {
+        const imageKit = new ImageKit({
+            publicKey: `${dotenv.config().parsed.IMAGEKIT_PUB}`,
+            privateKey: `${dotenv.config().parsed.IMAGEKIT_PRIV}`,
+            urlEndpoint: `${dotenv.config().parsed.IMAGEKIT_URL}`
+        })
+        try {
+            let response = imageKit.listFiles({
+                skip: skip,
+                limit: 2,
+                sort: 'DESC_CREATED'
+            })
+            return response
+        }
+        catch (error) {
+            console.info(error)
+            console.info(`\nErro ao listar arquivos\n`)
+            return null
+        }
+    }
+    downloadImage = async (image) => {
+        try {
+            let response = await axios.get(
+                image.url,
+                { responseType: 'stream' }
+            )
+            if (
+                response.hasOwnProperty('status') == true
+                && response.hasOwnProperty('data') == true
+                && response.status == 200
+            ) {
+                const writer = fs.createWriteStream(
+                    `${dotenv.config().parsed.DIR_BACKUP}/${image.name}`
+                )
+                response.data.pipe(writer)
+                return new Promise((resolve, reject) => {
+                    writer.on('finish', resolve(1))
+                    writer.on('error', reject(0))
+                })
+            }
+            else {
+                throw new Error(response)
+            }
+
+        }
+        catch (error) {
+            console.info(error)
+            console.info(`\nErro para download e/ou gravação do arquivo\n`)
+            return null
+        }
+    }
+
+    saveImage = async (response, image) => {
+        try {
+            
+        } catch (error) {
+            console.info(`\nErro ao salvar arquivo ${image}\n`)
+            console.info(error)
+            return null
+        }
+    }
+
+    wait(ms) {
+        var start = new Date().getTime()
+        var end = start
+        while (end < start + ms) {
+            end = new Date().getTime()
+        }
+    }
+}
+
+export default ImageKitOps
---
+++ backend/imagekit.backup.js
@@ -0,0 +1,67 @@
+import inquirer from 'inquirer'
+import ImageKitOps from './controllers/imagekit.ops.js'
+
+const downloadImage = async () => {
+    console.info('\n')
+    console.info(`\t📷 IMAGEKIT BACKUP`)
+    console.info(
+        `\t${new Date().toLocaleString('pt-BR', { hour12: false })}` +
+        `\n\tIniciado processo...\n`
+    )
+
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                name: 'method',
+                message: 'Choose the operation:',
+                choices: ['1 - START', '2 - CANCEL']
+            }
+        ])
+        .then(async method => {
+            if (method.method.split('-')[0] == 1) {
+                const imagekitOps = new ImageKitOps()
+                let skip = 0
+                let checkpoint = true
+                let files = null
+                let commit = null
+                let count = 0
+                while (checkpoint == true) {
+                    let response = await imagekitOps.listFiles(skip)
+                    while (!response == true || response == null) {
+                        imagekitOps.wait(1000 * 3)
+                        console.info(`\nEstou no WHILE - listagem - skip${skip}\n`)
+                        response = await imagekitOps.listFiles(skip)
+                    }
+                    if (Array.isArray(response) == true && response.length > 0) {
+                        skip += 1000
+                        for (let image of response) {
+                            files = await imagekitOps.downloadImage(image)
+                            while (!files == true || files == null || files == 0) {
+                                imagekitOps.wait(1000 * 3)
+                                console.info(`\nEstou no WHILE - download - skip${skip}\n`)
+                                files = await imagekitOps.downloadImage(image)
+                            }
+                            if (files == 1) {
+                                count += 1
+                            }
+                        }
+                    }
+                    else if (Array.isArray(response) == true && response.length == 0) {
+                        checkpoint = false
+                    }
+                    if (count % 1000 == 0) {
+                        console.info(`CONTAGEM = ${count}`)
+                    }
+                }
+            }
+            else if (method.method.split('-')[0] == 2) {
+                console.warn(`\nEND...\n`)
+            }
+            else {
+                console.warn(`\nEND...\n`)
+            }
+        })
+}
+
+downloadImage()
